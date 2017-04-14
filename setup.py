c87549862df972926b11020c23bbcabdb8915b57
Тимур Ахмадиев <akhmadiev@bars-open.ru>
2017-04-14 15:30:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('REQUIREMENTS', 'r') as f:
 
 setup(
     name="m3-spyne-smev",
-    version="0.1.13",
+    version="0.1.14",
     url="http://bitbucket.org/bars-group/spyne-smev",
     license='MIT',
     author='BARS Group',
