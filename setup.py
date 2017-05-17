c0334629374bd0e39ca6a1e36f5e0e41cc86f7b0
Андрей Чагочкин <andrey.chagochkin@bars-open.ru>
2017-05-17 18:07:49 +0700

    Подключение m3-builder.

--- setup.py
+++ setup.py
@@ -1,27 +1,39 @@
-# -*- coding: utf-8 -*-
-from distutils.core import setup
+# coding: utf-8
+import os
+
+from pip.download import PipSession
+from pip.req.req_file import parse_requirements
+from setuptools import setup, find_packages
 
 
-requires = []
-with open('REQUIREMENTS', 'r') as f:
-    requires.extend(f.readlines())
+def _get_requirements(file_name):
+    pip_session = PipSession()
+    requirements = parse_requirements(file_name, session=pip_session)
+
+    return tuple(str(requirement.req) for requirement in requirements)
+
+
+def _read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__),
+            fname)).read()
+    except IOError:
+        return ''
 
 setup(
-    name="m3-spyne-smev",
-    version="0.1.14",
-    url="http://bitbucket.org/bars-group/spyne-smev",
+    name='m3-spyne-smev',
+    description=u'Набор протоколов фреймворка spyne для работы со СМЭВ',
+    url='http://bitbucket.org/bars-group/spyne-smev',
     license='MIT',
     author='BARS Group',
-    description=u'Набор протоколов фреймворка spyne для работы со СМЭВ',
     author_email='bars@bars-open.ru',
-    package_dir={"": "src"},
-    packages=[
-        "spyne_smev", "spyne_smev.smev256", "spyne_smev.smev255",
-        "spyne_smev.server", "spyne_smev.wsse"],
-    package_data={"": ["xsd/*"]},
-    install_requires=requires,
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    description=_read('DESCRIPTION'),
+    install_requires=_get_requirements('REQUIREMENTS'),
+    long_description=_read('README.md'),
     include_package_data=True,
-    classifiers=[
+    classifiers=(
         'Intended Audience :: Developers',
         'Environment :: Web Environment',
         'Natural Language :: Russian',
@@ -30,5 +42,12 @@ setup(
         'Programming Language :: Python',
         'License :: OSI Approved :: MIT License',
         'Development Status :: 5 - Production/Stable',
-    ],
+    ),
+    dependency_links=(
+        'http://pypi.bars-open.ru/simple/m3-builder',
+    ),
+    setup_requires=(
+        'm3-builder>=1.0.1',
+    ),
+    set_build_info=os.path.dirname(__file__),
 )
